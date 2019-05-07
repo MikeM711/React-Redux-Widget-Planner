62b413a638cc8f7c9bf13f13d03ab98f173c08bc
Michael <michaelmccabe711@gmail.com>
2019-05-07 19:41:31 -0400

    finishing up controllers file

--- server/controllers/widgets.js
+++ server/controllers/widgets.js
@@ -3,7 +3,6 @@ const { widget } = require('../models')
 module.exports = {
   getWidgets: async (req, res, next) => {
     try {
-      console.log('inside controllers')
       const allWidgets = await widget.findAll()
       res.status(200).json({ allWidgets });
     }
@@ -11,5 +10,72 @@ module.exports = {
       console.log('error in finding widgets')
       res.status(400).json({ err });
     }
+  },
+
+  addWidget: async (req, res, next) => {
+    try {
+      const { newWidget } = req.body
+      // Hard-coded widget sent to database
+      const data = {
+        name: newWidget.name,
+        aluminum: newWidget.alum,
+        cold_rolled_steel: newWidget.crSteel,
+        galvanneal: newWidget.galv,
+        glass: newWidget.glass,
+        stainless_steel: newWidget.sSteel
+      }
+      const newWidgetRes = await widget.create(data)
+      console.log(newWidgetRes, 'successfully added')
+      // send data to the frontend
+      res.status(200).json({ data: newWidgetRes.dataValues });
+    }
+    catch (err) {
+      console.log(err, "widget was not successfully added to DB")
+      res.status(400).json({ err });
+    }
+  },
+
+  deleteWidget: async (req, res) => {
+    try {
+      const { id } = req.params
+      const delWidgetRes = await widget.destroy({ where: { id } })
+      // make sure only one widget was deleted
+      if (delWidgetRes === 1) {
+        // end successful connection
+        res.status(200).json({ success: true });
+      } else {
+        res.status(400).json({ err: 'Database did not delete correctly' })
+      }
+    }
+    catch (err) {
+      res.status(400).json({ err });
+    }
+  },
+
+  updateWidget: async (req, res) => {
+    try {
+      const { id, name, alum, crSteel, galv, glass, sSteel } = req.body.updatedWidget
+      const updateData = {
+        name: name,
+        aluminum: alum,
+        cold_rolled_steel: crSteel,
+        galvanneal: galv,
+        glass: glass,
+        stainless_steel: sSteel
+      }
+      const upWidgetResArr = await widget.update(updateData, { where: { id } })
+      // some security to make sure we only updated one row
+      if (upWidgetResArr.length === 1) {
+        // return the widget row we have updated
+        const updatedWidget = await widget.findOne({ where: { id } })
+        console.log('hello from UPDATED WIDGET response')
+        res.status(200).json({ data: updatedWidget.dataValues });
+      }
+    }
+    catch (err) {
+      console.log("Error in updating the database")
+      res.status(400).json({ err });
+    }
   }
+
 }
--- server/routes/widget/index.js
+++ server/routes/widget/index.js
@@ -1,76 +1,18 @@
 
 const router = require('express-promise-router')()
 
-const { widget } = require('../../models')
 const WidgetsController = require('../../controllers/widgets')
 
 router.route('/widgets')
   .get(WidgetsController.getWidgets);
 
-router.post('/widgetPOST', async (req, res) => {
-  try {
-    const { newWidget } = req.body
-    // Hard-coded widget sent to database
-    const data = {
-      name: newWidget.name,
-      aluminum: newWidget.alum,
-      cold_rolled_steel: newWidget.crSteel,
-      galvanneal: newWidget.galv,
-      glass: newWidget.glass,
-      stainless_steel: newWidget.sSteel
-    }
-    const newWidgetRes = await widget.create(data)
-    console.log(newWidgetRes, 'successfully added')
-    // send data to the frontend
-    res.status(200).json({ data: newWidgetRes.dataValues });
-  }
-  catch (err) {
-    console.log(err, "widget was not successfully added to DB")
-    res.status(400).json({ err });
-  }
+router.route('/widgetPOST')
+  .post(WidgetsController.addWidget)
 
-})
+router.route('/widgetDELETE/:id')
+  .delete(WidgetsController.deleteWidget)
 
-router.delete('/widgetDELETE/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const delWidgetRes = await widget.destroy({ where: { id } })
-    // make sure only one widget was deleted
-    if (delWidgetRes === 1) {
-      // end successful connection
-      res.status(200).json({ success: true });
-    } else {
-      res.status(400).json({ err: 'Database did not delete correctly' })
-    }
-  }
-  catch (err) {
-    res.status(400).json({ err });
-  }
-})
-
-router.put('/widgetUPDATE', async (req, res) => {
-  try {
-    const { id, name, alum, crSteel, galv, glass, sSteel } = req.body.updatedWidget
-    const updateData = {
-      name: name,
-      aluminum: alum,
-      cold_rolled_steel: crSteel,
-      galvanneal: galv,
-      glass: glass,
-      stainless_steel: sSteel
-    }
-    const upWidgetResArr = await widget.update(updateData, { where: { id } })
-    // some security to make sure we only updated one row
-    if (upWidgetResArr.length === 1) {
-      // return the widget row we have updated
-      const updatedWidget = await widget.findOne({ where: { id } })
-      res.status(200).json({ data: updatedWidget.dataValues });
-    }
-  }
-  catch (err) {
-    console.log("Error in updating the database")
-    res.status(400).json({ err });
-  }
-})
+router.route('/widgetUPDATE')
+  .put(WidgetsController.updateWidget)
 
 module.exports = router;
